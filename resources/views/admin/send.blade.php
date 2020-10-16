a826d9a8f3313cb9b0fb832b5ee2c03d62047628
phamthithanhbinhlanytao <41871874+phamthithanhbinhlanytao@users.noreply.github.com>
2020-10-16 10:43:19 +0630

    Create send.blade.php
    hello world

---
+++ resources/views/admin/send.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Gửi mail</title>
+</head>
+
+<body>
+<p> đơn hàng được gửi cho khách hàng: {{$name}}</p>
+<p> Đường dẫn đơn hàng : {{$body}}</p>
+<table width="400">
+  <tbody>
+    <tr>
+      <th>Tên sản phẩm</th>
+      <th>Giá sản phẩm</th>
+      <th>Số lượng</th>
+      <th>Mã giảm giá</th>
+      <th>Thành tiền</th>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  
+</body>
+
+</html>
